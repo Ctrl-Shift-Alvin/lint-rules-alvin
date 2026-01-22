00b4480e5dcfab2b50da567ebcbb2fe14554b6c6
Ctrl-Shift-Alvin <mail@alvinszoke.eu>
2026-01-22 22:33:35 +0100

    Migrate markdown plugin to '@eslint/markdown'. Bump versions

--- eslint/configs/markdown.ts
+++ eslint/configs/markdown.ts
@@ -1,11 +1,11 @@
 import { TSESLint } from '@typescript-eslint/utils';
-import eslintPluginMarkdown from 'eslint-plugin-markdown';
+import eslintPluginMarkdown from '@eslint/markdown';
 
 /**
  * The ESLint markdown config. Extends `configs.recommended` only for `md` files.
  */
 export const markdown: TSESLint.FlatConfig.Config = {
+	...eslintPluginMarkdown.configs.recommended[0],
 	name: 'eslint-plugin-markdown',
-	files: [ '**/*.{md}' ],
-	...eslintPluginMarkdown.configs.recommended
+	files: [ '**/*.md' ]
 };
